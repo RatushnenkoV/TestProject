8b62e9fa1a17dc43ffe8415291ea314edbbe75ee
ratushnenkovv <ratushnenkovv@mascon.vl.ru>
2018-11-06 16:44:21 +1000

    Добавлена модель сфдс

---
+++ WpfApp1/WpfApp1/src/Calc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Calc
+    {
+        double a;
+        double b;
+
+        double Add()
+        {
+            return a + b;
+        }
+
+        double Sub()
+        {
+            return a - b;
+        }
+
+        double Div()
+        {
+            return a / b;
+        }
+
+        double Mult()
+        {
+            return a * b;
+        }
+    }
+}
